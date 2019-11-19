87c1916b209c929dc38401dd2f06938515364416
Emily Haynes <ehayne00@hotmail.com>
2019-11-19 12:44:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,9 @@
-# Add your code here
+def help(songs)
+  puts "I accept the following commands:
+  - help: displays this help message
+  - list: displays a list of songs you can displays
+  - play: lets you choose a song to play
+  - exit: exits this program"
+end
+
+
